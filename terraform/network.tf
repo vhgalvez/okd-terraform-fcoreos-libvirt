fc23b9230b981f1e442a2c52672a61a3ff37bc61
Victor Galvez <vhgalvez@gmail.com>
2025-12-05 14:56:18 +0100

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,22 +1,24 @@
 # terraform/network.tf
 
 resource "libvirt_network" "okd_net" {
-  name      = "okd-net"
+  name      = var.network_name       # "okd-net"
   mode      = "nat"
   bridge    = "virbr_okd"
-  domain    = "okd.internal"
+  domain    = "okd-lab.cefaslocalserver.com"
+  addresses = [var.network_cidr]     # "10.56.0.0/24"
   autostart = true
-
-  # Nueva red OKD
-  addresses = ["10.56.0.0/24"]
 
   dhcp {
     enabled = true
   }
 
-  # Activar dnsmasq para resolución interna
+  # DNS de libvirt (10.56.0.1) → reenvía TODO a infra (10.56.0.10)
   dns {
     enabled    = true
     local_only = false
+
+    forwarders {
+      address = "10.56.0.10"   # VM infra con CoreDNS
+    }
   }
 }
