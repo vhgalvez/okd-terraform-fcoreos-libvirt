993b66f477b82c331374795122a59babc4421c36
Victor Galvez <vhgalvez@gmail.com>
2025-12-07 06:12:03 +0100

    Update ignition.tf

--- terraform/ignition.tf
+++ terraform/ignition.tf
@@ -2,19 +2,19 @@
 # Cargar Ignition generada por openshift-install
 
 resource "libvirt_ignition" "bootstrap" {
-  name      = "bootstrap.ign"
-  pool_name = libvirt_pool.okd.name
-  content   = file("${path.module}/../generated/ignition/bootstrap.ign")
+  name    = "bootstrap.ign"
+  pool    = libvirt_pool.okd.name
+  content = file("${path.module}/../generated/ignition/bootstrap.ign")
 }
 
 resource "libvirt_ignition" "master" {
-  name      = "master.ign"
-  pool_name = libvirt_pool.okd.name
-  content   = file("${path.module}/../generated/ignition/master.ign")
+  name    = "master.ign"
+  pool    = libvirt_pool.okd.name
+  content = file("${path.module}/../generated/ignition/master.ign")
 }
 
 resource "libvirt_ignition" "worker" {
-  name      = "worker.ign"
-  pool_name = libvirt_pool.okd.name
-  content   = file("${path.module}/../generated/ignition/worker.ign")
+  name    = "worker.ign"
+  pool    = libvirt_pool.okd.name
+  content = file("${path.module}/../generated/ignition/worker.ign")
 }
