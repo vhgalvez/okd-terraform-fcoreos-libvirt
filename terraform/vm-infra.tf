06821ad1325f90fa2d7f6656a56cdefa9b3f70c2
Victor Galvez <vhgalvez@gmail.com>
2025-12-08 11:31:22 +0100

    Update vm-infra.tf

--- terraform/vm-infra.tf
+++ terraform/vm-infra.tf
@@ -79,9 +79,6 @@ resource "libvirt_domain" "infra" {
     boot_devices = [{ dev = "hd" }]
   }
 
-  # Conecta el disco de cloud-init
-  cloudinit = libvirt_cloudinit_disk.infra_init.id
-
   devices = {
     disks = [
       {
@@ -95,6 +92,17 @@ resource "libvirt_domain" "infra" {
           dev = "vda"
           bus = "virtio"
         }
+      },
+      {
+        # Attach the cloud-init disk as a second disk
+        source = {
+          file = libvirt_cloudinit_disk.infra_init.id
+        }
+        target = {
+          dev = "vdb"
+          bus = "virtio"
+        }
+        readonly = true
       }
     ]
 
