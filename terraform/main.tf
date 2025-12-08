93aa6c559da9fd8b8a640f563be32493b19f7d59
Victor Galvez <vhgalvez@gmail.com>
2025-12-08 12:32:09 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.9.1"
+      version = "0.9.0"
     }
     template = {
       source  = "hashicorp/template"
