af0b96bed9200383edaaf7d050161b1ea800996d
Victor Galvez <vhgalvez@gmail.com>
2025-12-08 18:06:34 +0100

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -5,12 +5,7 @@ resource "libvirt_network" "okd_net" {
   bridge    = "virbr_okd"
   domain    = "${var.cluster_name}.${var.cluster_domain}"
   autostart = true
-
   addresses = [var.network_cidr]
-
-  dhcp {
-    enabled = true
-  }
 
   dns {
     enabled = true
