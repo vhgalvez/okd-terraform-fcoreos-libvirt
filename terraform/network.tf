b67dee6166286b8c878c336b6b4f1ddd50555175
Victor Galvez <vhgalvez@gmail.com>
2025-12-08 17:24:41 +0100

    Update network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,16 +1,19 @@
 # terraform\network.tf
+###########################################################
+# RED OKD SIN DHCP (modo route = IPs fijas 100% funcionales)
+###########################################################
+
 resource "libvirt_network" "okd_net" {
   name      = var.network_name
-  mode      = "nat"
+  mode      = "route"   # ← SOLUCIÓN DEFINITIVA PARA IP FIJA
   domain    = "${var.cluster_name}.${var.cluster_domain}"
 
+  # Rango de red
   addresses = [var.network_cidr]
+
   autostart = true
 
-  dhcp {
-    enabled = true
-  }
-
+  # DNS interno (opcional)
   dns {
     enabled = true
   }
