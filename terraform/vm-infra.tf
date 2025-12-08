b039559d6c8e164ff867a1f13042dfd822ff7f82
Victor Galvez <vhgalvez@gmail.com>
2025-12-09 00:10:17 +0100

    Update vm-infra.tf

--- terraform/vm-infra.tf
+++ terraform/vm-infra.tf
@@ -17,12 +17,14 @@ data "template_file" "infra_cloud_init" {
 
   vars = {
     hostname       = var.infra.hostname
+    short_hostname = split(".", var.infra.hostname)[0]
     ip             = var.infra.ip
     gateway        = var.gateway
     dns1           = var.dns1
     dns2           = var.dns2
     cluster_domain = var.cluster_domain
     cluster_name   = var.cluster_name
+    cluster_fqdn   = "${var.cluster_name}.${var.cluster_domain}"
     ssh_keys       = join("\n", var.ssh_keys)
     timezone       = var.timezone
   }
